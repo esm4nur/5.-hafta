5a66386c0df18b3a3e86226d8031c84c671166db
esm4nur <153914977+esm4nur@users.noreply.github.com>
2024-01-17 19:45:22 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Bedirhan Yıldız</h1>
+      <p>
+      25 Ekim 2004 tarihinde Antalya'da doğdum. Lise eğitimimi açık öğretim sistemiyle tamamladım ve şu anda üniversiteye giriş sınavlarına hazırlanıyorum.
+
+Çocukluğumdan beri teknolojiye ve bilime büyük bir ilgi duyuyorum. Özellikle yapay zeka ve yazılım geliştirme konularında kendimi geliştirmeye çalışıyorum. Bilgisayar başında geçirdiğim saatler, yeni teknolojilere ve programlamaya olan tutkumu daha da pekiştirdi. Yazılım dünyasında kendi izimi bırakmak, sorunlara yaratıcı çözümler bulmak beni motive ediyor.
+
+Futbol, enerjimi ve rekabet duygumu canlı tuttuğum bir diğer önemli ilgi alanımdır. Hem izlemekten hem de oynamaktan büyük keyif alıyorum. Takım çalışması, strateji ve hedeflere ulaşma arzusu, futbolu benim için daha da çekici kılıyor.
+
+Gelecekte, hem yazılım dünyasında hem de kişisel gelişimimde kendimi daha da ileri taşıyarak, tutkularımı birleştirebileceğim inovatif projelere imza atmayı hedefliyorum. Hayallerim ve hedeflerim doğrultusunda ilerlemeye devam ederken, öğrenmeye ve keşfetmeye her zaman açık bir birey olarak, bilgi ve deneyim kazanmaya devam ediyorum.
+      </p>
+    </>
+  )
+}
+
+export default App
